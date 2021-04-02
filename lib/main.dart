fff0fb315e5593202a3fb2f91ffb8b8ec85738db
Prabesh789 <raiprabesh789@gmail.com>
2021-04-02 22:42:06 +0545

    changes in main

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:sharebook/blocs/auth/auth_bloc.dart';
 import 'package:sharebook/global/componenets/const.dart';
 import 'package:sharebook/screens/login_screen/login_page.dart';
+import 'package:sharebook/screens/user_main_dashboard/user_main_dashboard.dart';
+import 'package:sharebook/utils/di.dart';
 import 'package:sharebook/utils/init.dart';
 
 void main() async {
@@ -8,7 +12,18 @@ void main() async {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  void initState() {
+    inject<AuthBloc>().add(IsAuthenticatedEvent());
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -20,7 +35,17 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: LoginPage(),
+      home: BlocConsumer<AuthBloc, AuthState>(
+        listener: (context, state) {},
+        builder: (context, state) {
+          if (state is AuthenticatedState) {
+            return UserMainDashboard();
+          } else if (state is UnAuthenticatedState) {
+            return LoginPage();
+          } else
+            return LoginPage();
+        },
+      ),
     );
   }
 }
